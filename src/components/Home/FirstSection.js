be229d6fc3a0d36f8e315e9b9673c8eb33a3115e
Aravind Balaji <61358471+aravind-cod-101@users.noreply.github.com>
2021-07-07 16:50:12 +0530

    Carousel images

--- src/components/Home/FirstSection.js
+++ src/components/Home/FirstSection.js
@@ -29,11 +29,38 @@ const FirstSection = () => {
           </div>
         </Carousel.Caption>
       </Carousel.Item>
-
       <Carousel.Item>
         <img
           className="d-block w-100 slider-image"
-          src={image3}
+          src="https://drive.google.com/uc?export=view&id=1O8cNLY9TvT0QCo5cqlO5nhqSO7SvgJZo"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 slider-image"
+          src="https://drive.google.com/uc?export=view&id=16_0J1_KjnwPxLuKZT3N-_EPBWZC-j9z9"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 slider-image"
+          src="https://drive.google.com/uc?export=view&id=166zysXyxx9Ybbxq2QaaL6a1a_UCCBnCT"
+          alt="Second slide"
+        />
+      </Carousel.Item>
+      <Carousel.Item>
+        <img
+          className="d-block w-100 slider-image"
+          src="https://drive.google.com/uc?export=view&id=1whCTVIrsU6n0txklR2E_KdEUcbkpg1E1"
+          alt="Second slide"
+        />
+      </Carousel.Item>{" "}
+      <Carousel.Item>
+        <img
+          className="d-block w-100 slider-image"
+          src="https://drive.google.com/uc?export=view&id=1DxWuiukgtFUZECLih4MaO8KhuY748KsH"
           alt="Second slide"
         />
       </Carousel.Item>
